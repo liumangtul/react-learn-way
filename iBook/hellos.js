2ae2869958557c0baf8382004ea9225a457c3acc
promise <promiseid@126.com>
2016-12-12 00:20:11 +0800

    redux

---
+++ iBook/hellos.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {findDOMNode} from 'react-dom';
+class Hello extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            like:'电脑游戏'
+        }
+    }
+
+    handleCLick(a,b,event){
+        this.setState({like:'全境封锁'});
+        console.log(a,b,event)
+    }
+
+    componentWillMount(){
+            console.log(`componentWillMount:
+            只会在装载之前调用一次，
+            在 render 之前调用，
+            你可以在这个方法里面调用 setState 改变状态，
+            并且不会导致额外调用一次 render`)
+    }
+
+    componentDidMount(){
+        //组件的dom
+        console.log(findDOMNode(this))
+        //组件内的refs访问dom元素
+        console.log('组件内的refs访问dom元素',this.refs.test)
+        this.refs.test.innerHTML='test';
+
+        console.log(`componentDidMount:
+            只会在装载完成之后调用一次，
+        在 render 之后调用，
+        从这里开始可以通过 ReactDOM.findDOMNode(this) 获取到组件的 DOM 节点。`)
+    }
+
+
+    render(){
+        return <div>我叫{this.props.name},今年{this.props.age}岁。我喜欢{this.state.like} <button onClick={this.handleCLick.bind(this,'A',this.state)}>button</button><div ref="test"></div><div ref="test"></div></div>
+    }
+}
+export {Hello};
--- iBook/iBook.js
+++ iBook/iBook.js
@@ -1,11 +1,38 @@
-import React,{Component} from 'react';
-import {reactDOM} from 'react-dom';
+import React from 'react';
+import reactDOM from 'react-dom';
+import {Hello} from './hellos';
 
-class Hello extends Component{
-    render(){
-        return <div>{this.props.name}</div>
+
+//关于 Class 继承
+class Promise {
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+
+    }
+
+    speak(){
+        return `我的名字叫${this.name},我今年${this.age}岁`;
     }
 }
+var mine=new Promise('王岩','24');
+//alert(mine.speak());
+
+class Yixiang extends Promise{
+
+    talk(){
+        return `${this.name}`
+    }
+}
+var she=new Yixiang('liumangtu',1);
+//alert(she.talk())
+//alert(she.speak())
+var props={
+    name:'wangyan',
+    age:24
+}
+
+
 reactDOM.render(
-    <Hello/>,document.getElementById('app')
+    <Hello {...props}/>,document.getElementById('app')
 );
