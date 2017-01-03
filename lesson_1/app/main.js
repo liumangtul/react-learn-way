261c19d7d18ae285fd5649d4e54929c7c336ef45
wangyan <promiseid@126.com>
2017-01-03 18:25:46 +0800

    react之路-lesson3->进阶3：react | react-router | redux | react-redux | webpack | webpack-dev-server 基本应用

--- lesson_1/app/main.js
+++ lesson_1/app/main.js
@@ -3,12 +3,42 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './Component/App';
 import store from './store';
+import {Router,Route,hashHistory,Link} from 'react-router';
 
 //Render
 const rootElement=document.getElementById('app');
+
+class Nav extends React.Component{
+    render(){
+        return(
+            <div>
+                <Link to="/index"> Index </Link>
+                <Link to="/app"> App </Link>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+class Index extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Index Page</h1>
+            </div>
+        )
+    }
+}
+
+
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <Router history={hashHistory}>
+            <Route path="/" component={Nav}>
+                <Route path="/index" component={Index}/>
+                <Route path="/app" component={App} />
+            </Route>
+        </Router>
     </Provider>,
     rootElement
 );
